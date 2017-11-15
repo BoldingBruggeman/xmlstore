e4d804944eaf93170a3123b02d69724eb38da127
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2017-11-15 14:48:22 +0000

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='xmlstore',
-      version='0.9.2',
+      version='0.9.3',
       description=' --- update - ',
       long_description=readme(),
       url='http://github.com/BoldingBruggeman/xmlstore',
